6c6370cf2229949396dfd8beed69860c1e0ea93d
zahir maliqi <zahir.maliqi@studentet.uni-pr.edu>
2022-07-15 23:04:04 +0200

    setState

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,22 +1,39 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 //import style 
 import './Home.css'
 
-const Home = () => {
+class Home extends Component{
+    constructor() {
+        super()
+        this.state={
+            talkmssg: "Talk with a doctor using our highly secured HIPAA complaint end-to-end encrypted video call.",
+            
+        }
+
+    }
+    changeText = () => {
+        this.setState({
+            talkmssg: "We will get back to you sooner ...",
+           
+        })
+    }
+
+    render(){
     return(
         <div className='hero' id='home-section'>
         <div className='content'>
             <h1>Consult a doctor anytime, anywhere by video call</h1>
-            <p className='hero-text'> Talk with a doctor using our highly secured HIPAA complaint end-to-end encrypted video call. </p>
+            <p className='hero-text'> { this.state.talkmssg }</p>
             <div className='hero-btn'>
-            <button className='ask-btn'>Ask A Doctor Online</button>
+            <button className='ask-btn' onClick={this.changeText}>Ask A Doctor Online</button>
             <button className='chat-btn'> Unlimited Chat</button>
             </div>
         </div>
       </div>
 
-    )
+       )
+     }
 }
 
 
