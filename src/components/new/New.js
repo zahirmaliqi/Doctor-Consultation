d831b24157fa45b80b7c770ef0c79d8f9303d35a
zahir maliqi <zahir.maliqi@studentet.uni-pr.edu>
2022-07-16 22:36:41 +0200

    adding news - List Rendering

--- src/components/new/New.js
+++ src/components/new/New.js
@@ -1,9 +1,33 @@
 import React from 'react'
 import './New.css'
-const New = () => {
+import News from './News';
+
+function New(){
+
+    const news =[
+        {
+        tittle: "New 1",
+        content:"The contents of a book is the list of chapters or articles or parts that are in the book, with the number of the page they begin on: If you look at the contents, you'll see there's a chapter on Japanese folk music."
+        },
+        {
+        tittle: "New 2",
+        content:"The contents of a book is the list of chapters or articles or parts that are in the book, with the number of the page they begin on: If you look at the contents, you'll see there's a chapter on Japanese folk music."
+        },
+        {
+        tittle: "New 3",
+        content:"The contents of a book is the list of chapters or articles or parts that are in the book, with the number of the page they begin on: If you look at the contents, you'll see there's a chapter on Japanese folk music."
+        }
+    ] ; 
+
+    const newList = news.map((list) => <News list={list}/>
+       );
+
+  
+      
 
     return (
         <div className='background-img'>
+           <div>{newList}</div>
            <div id='content'>
             <h1 >News</h1>
            <p>Your Healthier Today<br></br> & Better Tomorrow</p>
---
+++ src/components/new/News.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+function News({list}) {
+return(
+<div> <h2>{list.tittle}</h2> </div>
+
+)
+}
+
+export default News
